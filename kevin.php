061a57cb772223d452c986da02ba9b02e01c656e
bimbototo <136180316+bimbototo@users.noreply.github.com>
2024-10-29 02:18:12 +0700

    kevin.php

---
+++ kevin.php
@@ -0,0 +1,40 @@
+
+<script disable-devtool-auto src='https://cdn.jsdelivr.net/npm/disable-devtool@latest'></script>
+
+<style>
+::-webkit-scrollbar {
+    width: 1px;
+}
+ 
+::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.1); 
+    -webkit-border-radius: 0;
+    border-radius: 0;
+}
+ 
+::-webkit-scrollbar-thumb {
+    -webkit-border-radius: 0;
+    border-radius: 0;
+    background: rgba(0,0,0,0.1); 
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.1); 
+}
+::-webkit-scrollbar-thumb:window-inactive {
+  background: rgba(0,0,0,0.1); 
+}
+::-webkit-scrollbar-button {
+    background: transparent;
+}
+}
+
+</style>
+<div class="container" style="z-index: -1000; max-height: 3px;overflow: auto;background-color: transparent;"> 
+<ul>
+            <li><a href="https://mestia.gov.ge/">https://mestia.gov.ge/</a></li>
+            <li><a href="https://www.catering-delicious.com/">https://www.catering-delicious.com/</a></li>
+            <li><a href="https://stain-gp.ac.id/">https://stain-gp.ac.id/</a></li>
+            <li><a href="https://fashionsteps.gr/">https://fashionsteps.gr/</a></li>
+            <li><a href="https://qilin303.org/">https://qilin303.org/</a></li>
+            <li><a href="https://frankchaydez.com/">https://frankchaydez.com/</a></li>
+            <li><a href="https://95.111.194.82:1414/">https://95.111.194.82:1414/</a></li>
+</ul>
+</div>
